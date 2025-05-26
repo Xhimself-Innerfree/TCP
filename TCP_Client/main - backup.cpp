4122aa33b0d24bdbae67cba99c8713d3abd1633e
Xhimself-Innerfree <xhimselfinnerfree@gmail.com>
2025-05-26 10:50:29 -0400

    Global

---
+++ TCP_Client/main - backup.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#include <iostream>
+#include <winsock2.h>
+#include <iphlpapi.h>
+#include <ws2tcpip.h>
+#include <string>
+
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+//Client by JL
+
+int main() {
+	char IPv4_Server [100];
+	std::cout << "Client" << std::endl;
+	std::cout << "please input the server IP address: ";
+	std::cin >> IPv4_Server;
+
+	//Entrust in Windows Sockets
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	//Create a socket
+	SOCKET Client_Socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (INVALID_SOCKET == Client_Socket) {
+		printf("socket() failed\n");
+		return -1;
+	}
+
+	//Connect to the server
+	struct sockaddr_in target;
+	target.sin_family = AF_INET;  // AF_INET IPv4    AF_INET6 IPv6
+	target.sin_port = htons(6666); //the same as the server
+	target.sin_addr.s_addr = inet_addr(IPv4_Server); //IP address of the server
+
+	if (-1 == connect(Client_Socket, (struct sockaddr*)&target, sizeof(target))) {
+		printf("connect() failed\n");
+		closesocket(Client_Socket);
+		return -1;
+	}
+
+	//Start Communication
+	while (1) {
+		char sendbuf[1024] = { 0 };
+		printf("Please input the message to send: ");
+		scanf("%s", sendbuf);
+		send(Client_Socket, sendbuf, strlen(sendbuf), 0);
+
+		
+		char recvbuf[1024] = { 0 };
+		int rec = recv(Client_Socket, recvbuf, sizeof(recvbuf), 0);
+		if (rec <= 0) {
+			printf("recv() failed\n");
+			break;
+		}
+		printf("Received message: %s\n", recvbuf);
+		
+	}
+
+	//Close the socket
+	closesocket(Client_Socket);
+
+	return 0;
+}
--- TCP_Client/main.cpp
+++ TCP_Client/main.cpp
@@ -2,62 +2,86 @@
 #include <WinSock2.h>
 #pragma comment(lib, "ws2_32.lib")
 #include <iostream>
-#include <winsock2.h>
-#include <iphlpapi.h>
-#include <ws2tcpip.h>
-#include <string>
+#include <thread>
+#include <atomic>
 
-#pragma comment(lib, "iphlpapi.lib")
-#pragma comment(lib, "ws2_32.lib")
-//Client by JL
-
-int main() {
-	char IPv4_Server [100];
+int main()
+{
+	char IPv4_Server[100];
 	std::cout << "Client" << std::endl;
 	std::cout << "please input the server IP address: ";
 	std::cin >> IPv4_Server;
 
-	//Entrust in Windows Sockets
+	// Entrust in Windows Sockets
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	//Create a socket
+	// Create a socket
 	SOCKET Client_Socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (INVALID_SOCKET == Client_Socket) {
+	if (INVALID_SOCKET == Client_Socket)
+	{
 		printf("socket() failed\n");
 		return -1;
 	}
 
-	//Connect to the server
+	// Connect to the server
 	struct sockaddr_in target;
-	target.sin_family = AF_INET;  // AF_INET IPv4    AF_INET6 IPv6
-	target.sin_port = htons(8080); //the same as the server
-	target.sin_addr.s_addr = inet_addr(IPv4_Server); //IP address of the server
+	target.sin_family = AF_INET;
+	target.sin_port = htons(6666);
+	target.sin_addr.s_addr = inet_addr(IPv4_Server);
 
-	if (-1 == connect(Client_Socket, (struct sockaddr*)&target, sizeof(target))) {
+	if (-1 == connect(Client_Socket, (struct sockaddr *)&target, sizeof(target)))
+	{
 		printf("connect() failed\n");
 		closesocket(Client_Socket);
 		return -1;
 	}
 
-	//Start Communication
-	while (1) {
-		char sendbuf[1024] = { 0 };
-		printf("Please input the message to send: ");
-		scanf("%s", sendbuf);
-		send(Client_Socket, sendbuf, strlen(sendbuf), 0);
+	std::atomic<bool> running(true);
 
-		char recvbuf[1024] = { 0 };
-		int rec = recv(Client_Socket, recvbuf, sizeof(recvbuf), 0);
-		if (rec <= 0) {
-			printf("recv() failed\n");
+	// Thread for receiving messages
+	std::thread recv_thread([&]()
+							{
+        char recvbuf[1024] = { 0 };
+        while (running) {
+            int rec = recv(Client_Socket, recvbuf, sizeof(recvbuf) - 1, 0);
+            if (rec <= 0) {
+                printf("Server closed connection or recv() failed\n");
+                running = false;
+                break;
+            }
+            recvbuf[rec] = '\0';
+            printf("\nReceived message: %s\n", recvbuf);
+            printf("Please input the message to send: ");
+            fflush(stdout);
+        } });
+
+	// Main thread for sending messages
+	while (running)
+	{
+		char sendbuf[1024] = {0};
+		printf("Please input the message to send: ");
+		if (!fgets(sendbuf, sizeof(sendbuf), stdin))
+		{
+			running = false;
 			break;
 		}
-		printf("Received message: %s\n", recvbuf);
+		// Remove trailing newline
+		size_t len = strlen(sendbuf);
+		if (len > 0 && sendbuf[len - 1] == '\n')
+			sendbuf[len - 1] = '\0';
+		if (strcmp(sendbuf, "quit") == 0)
+		{
+			running = false;
+			break;
+		}
+		send(Client_Socket, sendbuf, strlen(sendbuf), 0);
 	}
 
-	//Close the socket
 	closesocket(Client_Socket);
-
+	running = false;
+	if (recv_thread.joinable())
+		recv_thread.join();
+	WSACleanup();
 	return 0;
 }
